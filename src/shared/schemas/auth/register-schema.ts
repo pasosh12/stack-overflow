bfecd8751b0c767c28986b81bc2351156eb990f6
Daniil <daniil.shishaev@gmail.com>
2025-07-03 00:03:11 +0300

    chore: delete logic

--- src/shared/schemas/auth/register-schema.ts
+++
@@ -1,7 +0,0 @@
-import {z} from 'zod';
-import {UserRoles} from "../../enums/user-roles.ts";
-export const UserDtoSchema = z.object({
-    id:z.string(),
-    username:z.string().min(3),
-    role:z.nativeEnum(UserRoles),
-})
