d4200d306efecb663361a7abd32450aefe123161
Daniil <daniil.shishaev@gmail.com>
2025-07-06 00:19:38 +0300

    feat: edit store and api connection

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {baseApi} from "../shared/api/base-api.ts";
+import {baseApi} from "@/shared/api/base-api";
+import {appReducer, appSlice} from "@/app/app-slice";
 
 export const store = configureStore({
     reducer:{
+        [appSlice.name]: appReducer,
         [baseApi.reducerPath]: baseApi.reducer
     },
     middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
