c718c954c2c325baf5175820d6c4a5df88976be6
Daniil <daniil.shishaev@gmail.com>
2025-07-10 13:12:03 +0300

    chore: postpage and homepage refactors

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client'
-import './index.css'
+
 import App from "@/app/App";
 import {store} from "@/app/store";
 import {Provider} from "react-redux";
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {usePublicSnippetQuery} from "@/modules/posts/model/posts-api";
 import {PostCard} from "@/modules/posts/ui/PostCard";
-import {selectIsAuthenticated} from "@/app/app-slice";
-import {useAppSelector} from "@/shared/hooks/use-app-selector";
+
 
 export const HomePage = () => {
     const {data: result, isLoading} = usePublicSnippetQuery({})
-    // console.log(result?.data)
-    const isAuthenticated = useAppSelector(selectIsAuthenticated)
-    console.log(isAuthenticated)
+
     if (isLoading) return <p>Loading...</p>
     return (
         <div>
