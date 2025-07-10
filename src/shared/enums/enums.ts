d675a6b16723560c67ea3de1deff278c8cba0cd6
Daniil <daniil.shishaev@gmail.com>
2025-07-10 13:13:14 +0300

    chore: reviews

---
+++ src/shared/enums/enums.ts
@@ -0,0 +1,6 @@
+export enum ResultCode {
+    Success = 200,
+    NoToken = 401,
+    UnprocessableEntity = 422
+
+}
--- src/shared/schemas/auth/user-dto-schema.ts
+++ src/shared/schemas/auth/user-dto-schema.ts
@@ -1,8 +1,7 @@
 import {z} from 'zod';
-import {UserRoles} from "@/shared/enums/user-roles";
 
 export const UserDtoSchema = z.object({
-    id:z.string(),
-    username:z.string().min(3),
-    role:z.nativeEnum(UserRoles),
+    id: z.string(),
+    username: z.string().min(3),
+    role: z.string()
 })
