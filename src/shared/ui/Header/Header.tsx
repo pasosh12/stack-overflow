47b604228d2afaa13076913f0d9ac5ba21bebbe0
Daniil <daniil.shishaev@gmail.com>
2025-07-10 12:48:29 +0300

    chore: header edited

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.css'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {useAppSelector} from "@/shared/hooks/use-app-selector";
 import {selectIsAuthenticated, setLogout} from "@/app/app-slice";
 import {useLogoutMutation} from "@/modules/auth";
@@ -11,6 +11,8 @@ export const Header = () => {
     const isAuth = useAppSelector(selectIsAuthenticated)
     const [logout] = useLogoutMutation()
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+
     const handleLogout = () => {
         logout().then(() => {
                 dispatch(setLogout())
@@ -21,16 +23,18 @@ export const Header = () => {
     }
     return (
         <header className={styles.header}>
-            <Link to="/" className={styles.logo}></Link>
+            <Link to="/" className={styles.logo}>Stack overflow</Link>
 
-            <nav className={styles.nav}>
-                <Link to="/">Home</Link>
-                <Link to="/create">Create post</Link>
-                <Link to="/profile">Profile</Link>
-            </nav>
+            {/*<nav className={styles.nav}>*/}
+            {/*    <Link to="/">Home</Link>*/}
+            {/*    <Link to="/create">Create post</Link>*/}
+            {/*    <Link to="/profile">Profile</Link>*/}
+            {/*</nav>*/}
 
             <div className={styles.actions}>
-                {isAuth ? <button onClick={handleLogout}>Log out</button> : null}
+                {isAuth ? <button onClick={handleLogout}>Log out</button> :
+                    <button onClick={()=>navigate('/login')}>
+                        Log in</button>}
             </div>
         </header>
     );
