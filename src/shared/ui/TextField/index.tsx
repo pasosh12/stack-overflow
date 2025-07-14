be9002939b730b881a83fec25b301b5cee1d46eb
Daniil <daniil.shishaev@gmail.com>
2025-07-14 16:44:56 +0300

    chore: replacements

--- src/shared/ui/TextField/index.tsx
+++ src/shared/ui/TextField/index.tsx
@@ -1,39 +1,44 @@
-import React, {InputHTMLAttributes, useId} from 'react';
+import React, {InputHTMLAttributes,
+    // TextareaHTMLAttributes
+    useId} from 'react';
 import clsx from 'clsx';
 import s from './TextField.module.css';
 
 type TextFieldType = {
-    variant?: 'outlined' | 'filled' | 'standard';
-    error?: boolean;
-    label?: string;
-    errorMessage?: string
-} & Omit<InputHTMLAttributes<HTMLInputElement>, 'placeholder'>;
+        variant?: 'outlined' | 'filled' | 'standard';
+        error?: boolean;
+        label?: string;
+        errorMessage?: string;
+        className?: string;
+        value?: string
+// } & TextareaHTMLAttributes<HTMLTextAreaElement>;
+    } & Omit<InputHTMLAttributes<HTMLInputElement>, 'placeholder'>;
 
 export function TextField({
                               variant = 'outlined',
                               label = 'Label',
                               className,
-                              // error = false,
+                              value,
                               errorMessage,
                               ...props
                           }: TextFieldType) {
-    const inputClassName = clsx(className, s.textfield_input, {
+    const inputClassName = clsx(s.textfield_input, {
         [s.textfield__standard]: variant === 'standard',
         [s.textfield__filled]: variant === 'filled',
         [s.textfield__outlined]: variant === 'outlined',
         [s.error]: !!errorMessage,
-    });
+    }, className);
     const generatedId = useId();
     const inputId = props.id ?? generatedId;
     return (
         <div className={s.textfield_wrapper}>
             <div className={s.input_wrapper}>
                 <input
+                    id={inputId}
                     className={inputClassName}
                     placeholder=" "
+                    value={value}
                     {...props}
-                    id={inputId}
-                    required={props.required}
                 />
                 {errorMessage ?
                     <label className={s.textfield_label} htmlFor={inputId}>
