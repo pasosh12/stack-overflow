780c39cf630e40f6b610946c4df06de1f93ea535
Daniil <daniil.shishaev@gmail.com>
2025-07-14 16:38:12 +0300

    feat: hook for navigating

--- src/shared/routing/Routing.tsx
+++ src/shared/routing/Routing.tsx
@@ -1,32 +1,32 @@
-import {selectIsAuthenticated} from "@/app/app-slice"
 import {Route, Routes} from "react-router"
-import {PageNotFound} from "@/shared/ui/PageNotFound/PageNotFound";
-import {useAppSelector} from "@/shared/hooks/use-app-selector";
-import {ProtectedRoute} from "@/shared/utils/protected-route";
 import {LoginPage} from "@/pages/LoginPage";
 import {HomePage} from "@/pages/HomePage";
 import {RegisterPage} from "@/pages/RegisterPage";
 import {SnippetPage} from "@/pages/SnippetsPage";
 import {ProfilePage} from "@/pages/ProfilePage";
+import {UsersPage} from "@/pages/UsersPage";
+import {CreateSnippetPage} from "@/pages/CreateSnippetPage";
+import {MySnippetsPage} from "@/pages/MySnippetPage";
+import {QuestionsPage} from "@/pages/QuestionsPage";
+import {CreateQuestionPage} from "@/pages/CreateQuestionPage";
+import {PageNotFound} from "@/pages/PageNotFound/PageNotFound";
+
+import {selectIsAuthenticated} from "@/app/app-slice"
+import {useAppSelector} from "@/shared/hooks";
+import {ProtectedRoute} from "@/shared/utils/protected-route";
+
 import {
     CREATE_QUESTION_PAGE_LINK,
     CREATE_SNIPPET_PAGE_LINK,
     HOME_PAGE_LINK,
     LOGIN_PAGE_LINK,
     MY_ACCOUNT_PAGE_LINK,
-    MY_QUESTIONS_PAGE_LINK,
     MY_SNIPPETS_PAGE_LINK,
     QUESTIONS_PAGE_LINK,
     REGISTER_PAGE_LINK,
     SNIPPET_PAGE_LINK,
     USERS_PAGE_LINK
 } from "@/shared/constants";
-import {UsersPage} from "@/pages/UsersPage";
-import {CreateSnippetPage} from "@/pages/CreateSnippetPage";
-import {MySnippetsPage} from "@/pages/MySnippetPage";
-import {QuestionsPage} from "@/pages/QuestionsPage";
-import {CreateQuestionPage} from "@/pages/CreateQuestionPage";
-import {MyQuestionsPage} from "@/pages/MyQuestions";
 
 export const Path = {
     Login: LOGIN_PAGE_LINK,
@@ -40,7 +40,6 @@ export const Path = {
     Users: USERS_PAGE_LINK,
     Questions: QUESTIONS_PAGE_LINK,
     CreateQuestion: CREATE_QUESTION_PAGE_LINK,
-    MyQuestions: MY_QUESTIONS_PAGE_LINK,
 } as const
 
 export const Routing = () => {
@@ -60,7 +59,6 @@ export const Routing = () => {
                 <Route path={Path.CreateSnippet} element={<CreateSnippetPage/>}/>
                 <Route path={Path.MySnippets} element={<MySnippetsPage/>}/>
                 <Route path={Path.CreateQuestion} element={<CreateQuestionPage/>}/>
-                <Route path={Path.MyQuestions} element={<MyQuestionsPage/>}/>
             </Route>
             <Route path={`${Path.Snippets}/:id`} element={<SnippetPage/>}/>
 
--- src/shared/ui/Sidebar/index.tsx
+++ src/shared/ui/Sidebar/index.tsx
@@ -1,42 +1,27 @@
 import s from './Sidebar.module.css'
 import {NavLink} from "react-router-dom";
-import {useAppSelector} from "@/shared/hooks/use-app-selector";
-import {selectIsAuthenticated} from "@/app/app-slice";
-import {
-    CREATE_SNIPPET_PAGE_LINK, HOME_PAGE_LINK,
-    MY_ACCOUNT_PAGE_LINK, MY_QUESTIONS_PAGE_LINK,
-    MY_SNIPPETS_PAGE_LINK,
-    QUESTIONS_PAGE_LINK,
-    USERS_PAGE_LINK
-} from "@/shared/constants";
+import clsx from "clsx";
+import {useFilteredNavItems} from "@/shared/hooks/use-filtered-nav-items";
 
 export const Sidebar = () => {
-    const navItems = [
-        {path: HOME_PAGE_LINK, label: "Home"},
-        {path: MY_ACCOUNT_PAGE_LINK, label: "My account", authOnly: true},
-        {path: CREATE_SNIPPET_PAGE_LINK, label: "Post Snippet", authOnly: true},
-        {path: MY_SNIPPETS_PAGE_LINK, label: "My Snippets", authOnly: true},
-        {path: QUESTIONS_PAGE_LINK, label: "Questions"},
-        {path: MY_QUESTIONS_PAGE_LINK, label: "My Questions", authOnly: true},
-        {path: USERS_PAGE_LINK, label: "Users"},
-    ];
-    const isLoggedIn = useAppSelector(selectIsAuthenticated);
-    const visibleLinks = isLoggedIn ? navItems : navItems.filter(item => !item.authOnly)
+    const filteredNavItems = useFilteredNavItems();
     return (
-        <aside className={s.sidebar}>
+        <aside className={clsx(s.sidebar)}>
             <nav>
                 <ul>
-                    {visibleLinks.map(({path,label})=>{
+                    {filteredNavItems.map(({path, label, icon}) => {
                         return (
-                        <li key={path}>
-                            <NavLink to={path} className={isActive=>isActive ? s.active : ''}>
-                                {label}
-                            </NavLink>
-                        </li>
-                    )})
+                            <li key={path}>
+                                <NavLink to={path} className={isActive => isActive ? s.active : ''}>
+                                    {icon}{label}
+                                </NavLink>
+                            </li>
+                        )
+                    })
                     }
                 </ul>
             </nav>
         </aside>
+
     )
 }
