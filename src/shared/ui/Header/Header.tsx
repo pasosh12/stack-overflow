96d02b163a33cb5b5203839c750dfe9609f29baa
Daniil <daniil.shishaev@gmail.com>
2025-07-14 16:48:36 +0300

    chore: header structure refactored

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,55 +1,50 @@
 import React from 'react';
 import styles from './Header.module.css'
 import {Link, useLocation, useNavigate} from 'react-router-dom';
-import {useAppSelector} from "@/shared/hooks/use-app-selector";
-import {selectIsAuthenticated, setLogout} from "@/app/app-slice";
-import {useLogoutMutation} from "@/modules/auth";
-import {useAppDispatch} from "@/shared/hooks/use-app-dispatch";
-import {baseApi} from "@/shared/api/base-api";
 import {Path} from "@/shared/routing";
+import {MobileSidebar} from "@/shared/ui/Sidebar/MobileSidebar";
+import {useMobileSidebar} from "@/shared/hooks/use-mobile-sidebar";
+import {HeaderActionButtons} from "@/shared/ui/Header/HeaderActionButtons";
+import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
 
 export const Header = () => {
-    const isAuth = useAppSelector(selectIsAuthenticated)
-    const [logout] = useLogoutMutation()
-    const dispatch = useAppDispatch()
+
+    const breakpoint: number = 600
     const navigate = useNavigate()
     const location = useLocation()
 
+    const {isMobile, toggleSidebar, mobileSidebarOpen, closeSidebar} = useMobileSidebar(breakpoint)
     const showCreateQuestionButton = location.pathname === Path.Questions
 
-    const handleLogout = () => {
-        logout().then(() => {
-            dispatch(setLogout())
 
-        }).then(() => {
-            dispatch(baseApi.util.invalidateTags(["Auth"]))
-        })
+    if (isMobile) {
+        return (
+            <>
+                <header className={styles.header}>
+                    <Link to="/" className={styles.logo}>Stack overflow</Link>
+                    <button className={styles.logo} onClick={toggleSidebar}> ☰</button>
+                </header>
+                <MobileSidebar isOpened={mobileSidebarOpen} toggleSidebar={toggleSidebar}
+                               closeSidebar={closeSidebar}
+                />
+            </>
+        )
     }
-
-return (
-    <header className={styles.header}>
-        <Link to="/" className={styles.logo}>Stack overflow</Link>
-
-        <div className={styles.actions}>
-            {showCreateQuestionButton && (
-                <button className={styles.header_action_button}
-                    onClick={()=>navigate(Path.CreateQuestion)}>
-                    Ask Question
-                </button>
-            )}
-            {isAuth ?
-                <button className={styles.header_action_button}
-                        onClick={handleLogout}>
-                    Log out
-                </button>
-                :
-                <button
-                    className={styles.header_action_button}
-                    onClick={() => navigate(Path.Login)}>
-                    Log in
-                </button>
-            }
-        </div>
-    </header>
-)}
+    return (
+        <header className={styles.header}>
+            <Link to="/" className={styles.logo}>
+                <CodeRoundedIcon/>
+                Stack overflow</Link>
+            <div className={styles.actions}>
+                {showCreateQuestionButton && (
+                    <button className={styles.header_action_button}
+                            onClick={() => navigate(Path.CreateQuestion)}>
+                        Ask Question
+                    </button>
+                )}
+                <HeaderActionButtons/>
+            </div>
+        </header>
+    )
+}
 
---
+++ src/shared/ui/Header/HeaderActionButtons/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from "@/shared/ui/Header/Header.module.css";
+import {Path} from "@/shared/routing";
+import {selectIsAuthenticated, setLogout} from "@/app/app-slice";
+import {baseApi} from "@/shared/api/base-api";
+import {useLogoutMutation} from "@/modules/auth";
+import {useAppDispatch} from "@/shared/hooks/use-app-dispatch";
+import {useAppSelector} from "@/shared/hooks/use-app-selector";
+import {useNavigate} from "react-router-dom";
+
+export const HeaderActionButtons = () => {
+    const [logout] = useLogoutMutation()
+    const isAuth = useAppSelector(selectIsAuthenticated)
+
+    const navigate = useNavigate()
+    const dispatch = useAppDispatch()
+
+    const handleLogout = () => {
+        logout().then(() => {
+            dispatch(setLogout())
+
+        }).then(() => {
+            dispatch(baseApi.util.invalidateTags(["Auth"]))
+        })
+    }
+    return (
+        <>
+            {isAuth ?
+                <button className={styles.header_action_button}
+                        onClick={handleLogout}>
+                    Log out
+                </button>
+                :
+                <button
+                    className={styles.header_action_button}
+                    onClick={() => navigate(Path.Login)}>
+                    Log in
+                </button>
+            }
+        </>
+    );
+};
